da9dae7600df11e3a4740ea5038252a17a26e4ba
RashadArbab <rashad.arbab@gmail.com>
2020-11-18 20:48:14 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import MPWindow from './MPWindow';
+
+
+function App() {
+
+
+
+  return (
+
+    <div className="App">
+      <h1 className= "Title"> 1 Bloor </h1>
+      <MPWindow/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/MPWindow.js
@@ -0,0 +1,19 @@
+
+
+function MPWindow() {
+
+    return (
+    
+    <div ClassName="MPWindow">
+        <iframe width="900" height="500"
+            src="https://my.matterport.com/show/?m=NZesGe74QKm&play=1"
+            frameborder="0" allowfullscreen allow="vr"
+            id="showcase_iframe">
+
+        </iframe>
+
+    </div>
+
+    )
+}
+export default MPWindow; 
